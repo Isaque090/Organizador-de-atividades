9972dd932d0e405e2605b35bf8ddfc9d098da126
Isaque <124292329+Isaque090@users.noreply.github.com>
2026-03-08 18:20:26 -0300

    Add files via upload

--- atividades.sql
+++ atividades.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08/03/2026 às 18:56
+-- Tempo de geração: 08/03/2026 às 22:19
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,7 +41,28 @@ CREATE TABLE `atividades` (
 INSERT INTO `atividades` (`id`, `ds_materia`, `dt_entrega`, `ds_atividade`) VALUES
 (1, 'Matemática', '2026-03-08', 'teste'),
 (2, 'teste', '2026-03-30', 'eeeeeeeeeeeeeeeeeeeeeeeeeeeee'),
-(3, 'teste  2', '2026-03-01', 'testeeeeeeeeeeeeeeeeeeeeeee');
+(3, 'teste  2', '2026-03-01', '          <form action=\"\" method=\"post\">\n                <label>Login</label>\n                <input class=\"form-control\" name=\"email\" type=\"text\" id=\"iiinput\" maxlength=\"20\" required>\n                 <label for=\"senha\">Senha</label>\n                 \n                 <input class=\"form-control\" name=\"senha\" type=\"password\" id=\"iiinput\" maxlength=\"20\" required>\n                <button name=\"login\" id=\"botao\" class=\"botao\" type=\"submit\">Começar</button>\n            </form>');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nm_nome` varchar(255) NOT NULL,
+  `ds_senha` varchar(255) NOT NULL,
+  `st_nivel` enum('normal','admin') NOT NULL,
+  `ds_email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nm_nome`, `ds_senha`, `st_nivel`, `ds_email`) VALUES
+(1, 'isaque', 'teste', 'admin', 'isaque@gmail.com');
 
 --
 -- Índices para tabelas despejadas
@@ -54,6 +75,13 @@ ALTER TABLE `atividades`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ds_email` (`ds_email`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
@@ -62,6 +90,12 @@ ALTER TABLE `atividades`
 --
 ALTER TABLE `atividades`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
